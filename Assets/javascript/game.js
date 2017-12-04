ef44b90b9b886d91ee40e3748a1f9f125e2bd2a9
jaro0024 <30360520+jaro0024@users.noreply.github.com>
2017-12-04 09:18:49 -0500

    added some divs to html and styled them on css and added some properties to the object on js document

---
+++ Assets/javascript/game.js
@@ -0,0 +1,41 @@
+// Global Variables
+
+var wins = 0;
+var losses = 0;
+
+//Hangman Object
+
+var hangman = {
+    words: [ "Baseball",
+    "Basketball",
+    "Football",
+    "Golf",
+    "Gymnastics",
+    "Handball",
+    "Hockey",
+    "Handball",
+    "Karate",
+    "Running",
+    "Sailing",
+    "Soccer",
+    "Swimming",
+    "Tennis",
+    "Volley Ball",
+    ],
+    letters: ["a", "b", "c", "d", "e",
+    "f", "g", "h", "i", "j", "k", "l",
+    "m", "n", "o", "p", "q", "r", "s",
+    "t", "u", "v", "x", "w", "y", "z",
+    ],
+    tries: 10,
+
+}
+
+// To start game
+
+
+
+    
+
+
+
