17fd340ed945b792bc2aebd15b824f377511bf9e
jaro0024 <30360520+jaro0024@users.noreply.github.com>
2017-12-05 17:25:37 -0500

    changed js to take letters that repeat in words into consideration and added losses div and changed the layout of html

--- Assets/javascript/game.js
+++ Assets/javascript/game.js
@@ -1,80 +1,63 @@
 
 // Global variables
+// ----------------------------------------------------
 
 // Array of words
-
 var words = ["baseball", "basketball", "football", "golf", "gymnastics", "handball", "hockey", "handball", "karate", "running", "sailing",
 "soccer", "swimming", "tennis", "volleyball"];
 
-
-var wins = 0;
-
+var randomWord;
+var remainingGuesses = 6;
 var currentWord = [];
 var guessedLetters = [];
 var underscore = [];
+var userGuesses =[];
+var winCount = 0;
+var lossCount = 0;
 
-// Choose words randomly
-
-var randomWord = words[Math.floor(Math.random() * words.length)];
-
-console.log(randomWord);
-
-// Main
-
-// Create underscore based on word's length
-
-var generateUnderscore = function () {
-    for(i = 0; i < randomWord.length; i++) {
+//To start game
+function startGame() {
+    randomWord = words[Math.floor(Math.random() * words.length)];
+    console.log(randomWord);
+    for(var i = 0; i < randomWord.length; i++) {
         underscore.push('_');
     }
-    return underscore;
+    document.getElementById('current-word').textContent = underscore.join(" ");
+    document.getElementById('remaining-guesses').textContent = remainingGuesses;
 }
 
-console.log(generateUnderscore());
-
-// Get user's guess 
-
+// To get user's guesses
 document.onkeypress = function(event) {
-    var letter = String.fromCharCode(event.keyCode).toLowerCase();
+    userGuesses = String.fromCharCode(event.keyCode).toLowerCase();
 
-// Check if guess is correct
-
-    if(randomWord.indexOf(letter) > -1) {
-        currentWord.push(letter);
-            console.log(currentWord);
-
-// If correct, replace underscore with letter
-        underscore[randomWord.indexOf(letter)] = letter;
-            console.log(underscore);
-
-            if(underscore.join('') == randomWord) {
-                alert("You Win!");
+    if(randomWord.indexOf(userGuesses) > -1) {
+        for(var i = 0; i < randomWord.length; i++) {
+            if(randomWord[i] === userGuesses)
+            {
+                underscore[i] = userGuesses;
+                console.log(underscore);
             }
+        }
     }
-
-// If incorrect, show the letter as letters guessed
     else {
-        guessedLetters.push(letter);
+        guessedLetters.push(userGuesses);
+            remainingGuesses--;
             console.log(guessedLetters);
     }
+}
 
-};
+// Win / lose 
 
+function winLose() {
+    
 
-
-// var str = 'To be, or not to be, that is the question.';
-// var count = 0;
-// var pos = str.indexOf('e');
-
-// while (pos !== -1) {
-//   count++;
-//   pos = str.indexOf('e', pos + 1);
-// }
-
-// console.log(count); // displays 4 
+// Main 
+// ------------------------------------------------------
+startGame();
 
 
-// If word is correct, increase win number  
+
+
 
 
 
