47f771cb37b8fe32c0e4c11e5c4dabdfed86728f
jaro0024 <30360520+jaro0024@users.noreply.github.com>
2017-12-06 15:23:54 -0500

    added the winLose function, fixed some issues with double letters and add the content to html (inner.HTML)

--- Assets/javascript/game.js
+++ Assets/javascript/game.js
@@ -1,71 +1,83 @@
 
+// need to end game, reset and restart while keeping the score
+// need to stop remaining guesses at 0 (probably by ending the game, this will be fixed)
+
+
+
+
 // Global variables
 // ----------------------------------------------------
 
 // Array of words
-var words = ["baseball", "basketball", "football", "golf", "gymnastics", "handball", "hockey", "handball", "karate", "running", "sailing",
-"soccer", "swimming", "tennis", "volleyball"];
+var wordsArray = ["baseball", "basketball", "football", "golf", "gymnastics", "handball", "hockey", "handball", "karate", "running", "sailing",
+    "soccer", "swimming", "tennis", "volleyball"];
 
 var randomWord;
 var remainingGuesses = 6;
-var currentWord = [];
 var guessedLetters = [];
 var underscore = [];
-var userGuesses =[];
-var winCount = 0;
-var lossCount = 0;
+var correctGuess = 0;
+var wins = 0;
+var losses = 0;
+var guess;
 
-//To start game
+// //To start game
 function startGame() {
-    randomWord = words[Math.floor(Math.random() * words.length)];
-    console.log(randomWord);
-    for(var i = 0; i < randomWord.length; i++) {
-        underscore.push('_');
-    }
-    document.getElementById('current-word').textContent = underscore.join(" ");
-    document.getElementById('remaining-guesses').textContent = remainingGuesses;
+randomWord = wordsArray[Math.floor(Math.random() * wordsArray.length)];
+console.log(randomWord);
+for (var i = 0; i < randomWord.length; i++) {
+    underscore.push('_');
+    document.getElementById("current-word").innerHTML = underscore.join(" ");
+}
 }
 
-// To get user's guesses
-document.onkeypress = function(event) {
-    userGuesses = String.fromCharCode(event.keyCode).toLowerCase();
-
-    if(randomWord.indexOf(userGuesses) > -1) {
-        for(var i = 0; i < randomWord.length; i++) {
-            if(randomWord[i] === userGuesses)
-            {
-                underscore[i] = userGuesses;
-                console.log(underscore);
+// To get user's guesses and check if it is correct
+document.onkeypress = function (event) {
+    guess = String.fromCharCode(event.keyCode).toLowerCase();
+    if (randomWord.indexOf(guess) > -1) {
+        for (var i = 0; i < randomWord.length; i++) {
+            if (randomWord[i] === guess) {
+                underscore[i] = guess;
+                correctGuess++;
+                winLose();
+                document.getElementById("current-word").innerHTML = underscore.join(" ");
             }
         }
     }
     else {
-        guessedLetters.push(userGuesses);
-            remainingGuesses--;
-            console.log(guessedLetters);
+        guessedLetters.push(guess);
+        remainingGuesses--;
+        winLose();
+        document.getElementById("guessed-letters").innerHTML = guessedLetters;
+        document.getElementById("remaining-guesses").innerHTML = remainingGuesses;
     }
 }
 
-// Win / lose 
+// Win / lose function
 
 function winLose() {
-    
+    if (correctGuess === randomWord.length) {
+        wins++;
+        document.getElementById("total-wins").innerHTML = wins;
+        // startGame();
+    }
+
+    else if (remainingGuesses === 0) {
+        losses++;
+        document.getElementById("total-losses").innerHTML = losses;
+    }
+
+
+}
+
+// Reset the game
+
+
 
 // Main 
 // ------------------------------------------------------
+
 startGame();
-
-
-
-
-
-
-
-
-
-
-
-    
 
 
 
